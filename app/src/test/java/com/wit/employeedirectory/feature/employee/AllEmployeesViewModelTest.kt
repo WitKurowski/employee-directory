b49ff31ebc5a3ba97ead83b16eb36ea1c7774d70
Wit Kurowski <witkurowski@gmail.com>
2025-03-31 13:13:28 -0700

    Add unit test to verify force refresh works.

--- app/src/test/java/com/wit/employeedirectory/feature/employee/AllEmployeesViewModelTest.kt
+++ app/src/test/java/com/wit/employeedirectory/feature/employee/AllEmployeesViewModelTest.kt
@@ -136,6 +136,69 @@ class AllEmployeesViewModelTest {
 	}
 
 	@Test
+	fun `replace employee list with newly fetched set of employees when refresh forced`() = runTest(testDispatcher) {
+		val id1 = "1"
+		val name1 = "Bob"
+		val photoUrlString1 = "http://photo.url.string/1.jpg"
+		val team1 = "Team 1"
+		val id2 = "2"
+		val name2 = "Sue"
+		val photoUrlString2 = "http://photo.url.string/2.jpg"
+		val team2 = "Team 2"
+		val id3 = "3"
+		val name3 = "Anne"
+		val photoUrlString3 = "http://photo.url.string/3.jpg"
+		val team3 = "Team 3"
+		val employeeState1 = EmployeeState(id1, name1, photoUrlString1, team1)
+		val employeeState2 = EmployeeState(id2, name2, photoUrlString2, team2)
+		val employeeState3 = EmployeeState(id3, name3, photoUrlString3, team3)
+		val initialEmployeeStates = listOf(employeeState3, employeeState2, employeeState1)
+		viewModel._employeeStatesFlow.value = initialEmployeeStates
+
+		val employees = listOf(
+			Employee(id = id1, name = name1, photoUrlString = photoUrlString1, team = team1),
+			Employee(id = id2, name = name2, photoUrlString = photoUrlString2, team = team2),
+			Employee(id = id3, name = name3, photoUrlString = photoUrlString3, team = team3)
+		)
+		given(employeesRepository.getAllEmployees()) //
+			.willReturn(employees)
+
+		val actualEmployeeStates = mutableListOf<List<EmployeeState>>()
+		backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
+			viewModel.employeeStatesFlow.toList(actualEmployeeStates)
+		}
+		val actualEmptyStates = mutableListOf<EmptyState>()
+		backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
+			viewModel.emptyStateFlow.toList(actualEmptyStates)
+		}
+		val actualErrorStates = mutableListOf<ErrorState>()
+		backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
+			viewModel.errorStateFlow.toList(actualErrorStates)
+		}
+		val actualLoadingStates = mutableListOf<LoadingState>()
+		backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
+			viewModel.loadingStateFlow.toList(actualLoadingStates)
+		}
+
+		viewModel.fetchAllEmployees(forceRefresh = true)
+		testDispatcher.scheduler.advanceUntilIdle()
+
+		val finalEmployeeStates = listOf(
+			employeeState1, employeeState2, employeeState3
+		)
+		assertEquals(listOf(initialEmployeeStates, finalEmployeeStates), actualEmployeeStates)
+		assertEquals(listOf(EmptyState(visible = false)), actualEmptyStates)
+		assertEquals(listOf(ErrorState(visible = false)), actualErrorStates)
+		assertEquals(
+			listOf(
+				LoadingState(visible = false),
+				LoadingState(visible = true),
+				LoadingState(visible = false)
+			), actualLoadingStates
+		)
+	}
+
+	@Test
 	fun `show error state when HttpException is thrown`() = runTest(testDispatcher) {
 		given(employeesRepository.getAllEmployees()) //
 			.willThrow(HttpException::class.java)
