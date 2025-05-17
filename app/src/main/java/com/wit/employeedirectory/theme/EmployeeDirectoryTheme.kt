5b5acd1d4249a06e43d814e76158b3f11efda9ed
Wit Kurowski <witkurowski@gmail.com>
2025-05-17 15:16:36 -0700

    Add support for dynamic color themes.

--- app/src/main/java/com/wit/employeedirectory/theme/EmployeeDirectoryTheme.kt
+++ app/src/main/java/com/wit/employeedirectory/theme/EmployeeDirectoryTheme.kt
@@ -1,25 +1,42 @@
 package com.wit.employeedirectory.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import com.wit.employeedirectory.R
 
 @Composable
 fun EmployeeDirectoryTheme(
-	darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit
+	darkTheme: Boolean = isSystemInDarkTheme(),
+	dynamicColor: Boolean = true,
+	content: @Composable () -> Unit
 ) {
-	val lightColors = lightColorScheme(
-		surface = colorResource(id = R.color.light_blue),
-	)
-	val darkColors = darkColorScheme(
-		surface = colorResource(id = R.color.dark_blue),
-	)
-	val colors = if (darkTheme) darkColors else lightColors
+	val colorScheme = when {
+		dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+			val context = LocalContext.current
+			if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+		}
+
+		darkTheme -> {
+			darkColorScheme(
+				surface = colorResource(id = R.color.dark_blue),
+			)
+		}
+
+		else -> {
+			lightColorScheme(
+				surface = colorResource(id = R.color.light_blue),
+			)
+		}
+	}
 	MaterialTheme(
-		colorScheme = colors, content = content
+		colorScheme = colorScheme, content = content
 	)
 }
