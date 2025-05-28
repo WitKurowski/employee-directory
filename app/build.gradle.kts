5f0771bf1aed560620e724e721debc5b3ef63f1a
Wit Kurowski <witkurowski@gmail.com>
2025-05-28 15:21:04 -0700

    Replace activity layout file with Compose `setContent()` function.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ android {
 }
 
 dependencies {
+	implementation(libs.androidx.activity.compose)
 	implementation(libs.androidx.appcompat)
 	implementation(libs.androidx.core.ktx)
 	implementation(libs.androidx.fragment.ktx)
--- app/src/main/java/com/wit/employeedirectory/feature/home/HomeActivity.kt
+++ app/src/main/java/com/wit/employeedirectory/feature/home/HomeActivity.kt
@@ -1,28 +1,22 @@
 package com.wit.employeedirectory.feature.home
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.fragment.app.FragmentActivity
-import com.wit.employeedirectory.databinding.ActivityHomeBinding
 import com.wit.employeedirectory.feature.employee.AllEmployeesScreen
 import com.wit.employeedirectory.theme.EmployeeDirectoryTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeActivity : FragmentActivity() {
-	private lateinit var viewBinding: ActivityHomeBinding
-
+class HomeActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
 		enableEdgeToEdge()
 
-		viewBinding = ActivityHomeBinding.inflate(layoutInflater)
-
-		setContentView(viewBinding.root)
-
 		// TODO: Find proper way to handle insets and padding when dealing with edge-to-edge.
-		viewBinding.composeView.setContent {
+		setContent {
 			EmployeeDirectoryTheme {
 				AllEmployeesScreen()
 			}
